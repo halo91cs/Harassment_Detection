d22a079143610bbc157491862ad05a5aa565a484
Hale Inan <41010793+halo91cs@users.noreply.github.com>
2020-09-16 16:07:00 -0400

    Removed unnecessary lines

--- MainEngine.py
+++ MainEngine.py
@@ -7,14 +7,9 @@ from RegexConfig import Configuration
 class MainEngine:
     ext_img = ExtractImages()
     trans_obj = Transformation()
-    ext_obj = Extract()
-    config = Configuration()
 if __name__ == '__main__':
     main_obj = MainEngine()
-    dct = main_obj.trans_obj.processTweets(main_obj.trans_obj.processHarassmentLexicon())
-    print(len(dct))
-   # main_obj.ext_obj
-    #main_obj.trans_obj.processHarassmentLexicon()
+    
     # Uncomment to download the profile images.. It should run only once..
     # main_obj.ext_img.download_profile_images()
 
@@ -24,5 +19,5 @@ if __name__ == '__main__':
     
     # this gives the final dataset..
     # final dataset is loaded into excel..
-    #final_dataset = main_obj.trans_obj.aggregated_dataset()
-    #final_dataset.to_excel("/home/hale/Documents/hale_test_projects/harassment_detection/output/final_dataset.xlsx")
+    final_dataset = main_obj.trans_obj.aggregated_dataset()
+    final_dataset.to_excel("~/final_dataset.xlsx")
